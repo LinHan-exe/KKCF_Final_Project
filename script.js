9e5d5ba2d9ddc292e5118c6ba20985a4b5694394
Lin Han Zhou <92904128+LinHan-exe@users.noreply.github.com>
2023-08-15 22:47:23 -0400

    Final project version commit via files

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-document.getElementById("uniquebutton1").onclick(declare());
-function declare(){
-  var p = prompt("Your Name");
-}
+// Define an array of button IDs
+const buttonIds = ["unique", "unique1", "button1", "button2", "button3"];
+
+// Function to toggle button color
+const toggleButtonColor = (button, isEntering) => {
+  button.querySelector("a").style.backgroundColor = isEntering ? '#3F5E5A' : '#2D2D2A';
+  button.style.backgroundColor = isEntering ? '#3F5E5A' : '#2D2D2A'; // Change color for hover
+};
+
+// Loop through button IDs and add event listeners
+buttonIds.forEach(buttonId => {
+  const button = document.getElementById(buttonId);
+  if (button) {
+    button.addEventListener("mouseenter", () => toggleButtonColor(button, true));
+    button.addEventListener("mouseleave", () => toggleButtonColor(button, false));
+  }
+});
+
+// Get the navbar buttons
+const homeButton = document.getElementById("button1");
+const businessButton = document.getElementById("button2");
+const contactButton = document.getElementById("button3");
+
+// Add event listeners for hover
+homeButton.addEventListener("mouseenter", () => toggleButtonColor(homeButton, true));
+homeButton.addEventListener("mouseleave", () => toggleButtonColor(homeButton, false));
+
+businessButton.addEventListener("mouseenter", () => toggleButtonColor(businessButton, true));
+businessButton.addEventListener("mouseleave", () => toggleButtonColor(businessButton, false));
+
+contactButton.addEventListener("mouseenter", () => toggleButtonColor(contactButton, true));
+contactButton.addEventListener("mouseleave", () => toggleButtonColor(contactButton, false));
